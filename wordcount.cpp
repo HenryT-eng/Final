4950889b9cde6d67aef8e6d2bad63b08e745cc44
HenryT-eng <ht607919@ohio.edu>
2022-04-26 11:48:30 -0400

    not working

---
+++ wordcount.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+string pName;
+
+int main(int argc, char **argv){
+
+return;
+}
+
+int countLine(string text){
+    int lines = 0;
+
+
+    cout << lines << " lines" << endl;
+    return lines;
+}
+
+int countChar(string text){
+    int characters = 0;
+
+
+
+    cout << characters << " characters." << endl;
+    return characters;
+}
